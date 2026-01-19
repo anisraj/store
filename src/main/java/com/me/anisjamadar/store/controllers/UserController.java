387b0bc4fdb694656e24ac7ae19acd3a0c92affe
anisraj <anisjamadar26@gmail.com>
2026-01-19 14:07:51 +0530

    using Data Transfer Objects

--- src/main/java/com/me/anisjamadar/store/controllers/UserController.java
+++ src/main/java/com/me/anisjamadar/store/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.me.anisjamadar.store.controllers;
 
+import com.me.anisjamadar.store.dtos.UserDto;
 import com.me.anisjamadar.store.entities.User;
 import com.me.anisjamadar.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping("/users")
@@ -16,16 +19,20 @@ public class UserController {
     private final UserRepository userRepository;
 
     @GetMapping
-    public Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+    public List<UserDto> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
+                .toList();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
         var user = userRepository.findById(id).orElse(null);
         if (user == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(user);
+        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
     }
 }
--- src/main/java/com/me/anisjamadar/store/repositories/UserRepository.java
+++ src/main/java/com/me/anisjamadar/store/repositories/UserRepository.java
@@ -2,12 +2,13 @@ package com.me.anisjamadar.store.repositories;
 
 import com.me.anisjamadar.store.entities.User;
 import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     
 }
