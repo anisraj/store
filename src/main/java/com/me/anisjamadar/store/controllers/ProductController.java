31988a40e6d988858e7c146e1df7372ba752dfea
anisraj <anisjamadar26@gmail.com>
2026-01-19 16:18:13 +0530

    Building Product Endpoints

---
+++ src/main/java/com/me/anisjamadar/store/controllers/ProductController.java
@@ -0,0 +1,44 @@
+package com.me.anisjamadar.store.controllers;
+
+import com.me.anisjamadar.store.dtos.ProductDto;
+import com.me.anisjamadar.store.entities.Product;
+import com.me.anisjamadar.store.mappers.ProductMapper;
+import com.me.anisjamadar.store.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    @GetMapping
+    public List<ProductDto> getAllProducts(
+         @RequestParam(required = false, name = "categoryId") Byte categoryId
+    ) {
+        List<Product> products;
+        if (categoryId != null) {
+            products = productRepository.findByCategoryId(categoryId);
+        } else {
+            products = productRepository.findAllWithCategory();
+        }
+        return products
+                .stream()
+                .map(productMapper::toDto)
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getProduct(@PathVariable Long id) {
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(productMapper.toDto(product));
+    }
+}
--- src/main/java/com/me/anisjamadar/store/repositories/ProductRepository.java
+++ src/main/java/com/me/anisjamadar/store/repositories/ProductRepository.java
@@ -1,9 +1,18 @@
 package com.me.anisjamadar.store.repositories;
 
 import com.me.anisjamadar.store.entities.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @EntityGraph(attributePaths = "category")
+    List<Product> findByCategoryId(Byte categoryId);
 
+    @EntityGraph(attributePaths = "category")
+    @Query("select p from Product p")
+    List<Product> findAllWithCategory();
 }
