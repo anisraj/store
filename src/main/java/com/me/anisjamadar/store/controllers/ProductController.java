63ca3aea6e74666846818b9d73300d4f50ff6208
anisraj <anisjamadar26@gmail.com>
2026-01-20 12:42:28 +0530

    Building a CRUD API for products

--- src/main/java/com/me/anisjamadar/store/controllers/ProductController.java
+++ src/main/java/com/me/anisjamadar/store/controllers/ProductController.java
@@ -3,10 +3,12 @@ package com.me.anisjamadar.store.controllers;
 import com.me.anisjamadar.store.dtos.ProductDto;
 import com.me.anisjamadar.store.entities.Product;
 import com.me.anisjamadar.store.mappers.ProductMapper;
+import com.me.anisjamadar.store.repositories.CategoryRepository;
 import com.me.anisjamadar.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -16,6 +18,7 @@ import java.util.List;
 public class ProductController {
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
 
     @GetMapping
     public List<ProductDto> getAllProducts(
@@ -40,5 +43,54 @@ public class ProductController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(productMapper.toDto(product));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDto> addProduct(
+        @RequestBody ProductDto productDto,
+        UriComponentsBuilder uriBuilder
+    ) {
+        var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null);
+        if (category == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        var product = productMapper.toEntity(productDto);
+        product.setCategory(category);
+
+        var result = productRepository.save(product);
+        var addedProductDto = productMapper.toDto(result);
+        var uri = uriBuilder.path("products/{id}").buildAndExpand(addedProductDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(addedProductDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> updateProduct(
+        @PathVariable(name = "id") Long id,
+        @RequestBody ProductDto productDto
+    ) {
+        var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null);
+        if (category == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        productMapper.update(productDto, product);
+        product.setCategory(category);
+        var result = productRepository.save(product);
+        return ResponseEntity.ok(productMapper.toDto(result));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable(name = "id") Long id) {
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        productRepository.delete(product);
+        return ResponseEntity.noContent().build();
     }
 }
