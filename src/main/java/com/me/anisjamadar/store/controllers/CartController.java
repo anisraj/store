eccd421bf847c86d5379bc4131e5de1b59a6fe11
anisraj <anisjamadar26@gmail.com>
2026-01-21 16:06:12 +0530

    getting cart

--- src/main/java/com/me/anisjamadar/store/controllers/CartController.java
+++ src/main/java/com/me/anisjamadar/store/controllers/CartController.java
@@ -41,7 +41,7 @@ public class CartController {
         @PathVariable UUID cartId,
         @Valid @RequestBody AddItemToCartRequest request
     ) {
-        var cart = cartRepository.findById(cartId).orElse(null);
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
         if (cart == null) {
             return ResponseEntity.notFound().build();
         }
@@ -51,7 +51,7 @@ public class CartController {
             return ResponseEntity.badRequest().build();
         }
 
-        var cartItem = cart.getCartItems().stream()
+        var cartItem = cart.getItems().stream()
                 .filter(item -> item.getProduct().getId().equals(product.getId()))
                 .findFirst()
                 .orElse(null);
@@ -63,11 +63,23 @@ public class CartController {
             cartItem.setProduct(product);
             cartItem.setQuantity(1);
             cartItem.setCart(cart);
-            cart.getCartItems().add(cartItem);
+            cart.getItems().add(cartItem);
         }
 
         cartRepository.save(cart);
         CartItemDto cartItemDto = cartMapper.toDto(cartItem);
         return ResponseEntity.status(HttpStatus.CREATED).body(cartItemDto);
+    }
+
+    @GetMapping("/{cartId}")
+    public ResponseEntity<CartDto> getCart(
+            @PathVariable UUID cartId
+    ) {
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
+        if (cart == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(cartMapper.toDto(cart));
     }
 }
